dd0f782047f236ef09a3dc09ed247da605c76f63
HyungJuu <rmsdk617@naver.com>
2024-05-28 12:35:15 +0900

    up

--- part2/win/SmartHomeMonitor/Views/RealTimeControl.xaml.cs
+++ part2/win/SmartHomeMonitor/Views/RealTimeControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using LiveChartsCore.SkiaSharpView.Extensions;
+﻿using LiveChartsCore.SkiaSharpView.Drawing;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.VisualElements;
 using MahApps.Metro.Controls;
 using MQTTnet;
 using MQTTnet.Client;
@@ -21,6 +24,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
 
 namespace SmartHomeMonitoringApp.Views
 {
@@ -46,6 +52,7 @@ namespace SmartHomeMonitoringApp.Views
 
         private async void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
+            // MQTT 설정
             if (Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
             {
                 // 이미 다른화면에서 MQTT를 연결했다면
@@ -65,7 +72,6 @@ namespace SmartHomeMonitoringApp.Views
                         f.WithTopic(Commons.MQTTTOPIC);
                     }).Build();
                 await Commons.MQTT_CLIENT.SubscribeAsync(mqttSubscribeObtions, CancellationToken.None);
-
             }
         }
 
@@ -76,6 +82,20 @@ namespace SmartHomeMonitoringApp.Views
             UpdateChart(payload);
 
             return Task.CompletedTask;
+        }
+
+        // 앵귤러차트를 위한 변수, 속성 선언
+        public IEnumerable<ISeries> HumidSeries { get; set; }
+
+        public IEnumerable<VisualElement<SkiaSharpDrawingContext>> VisualElements { get; set; }
+
+        public NeedleVisual Needle { get; set; }
+
+        private static void SetStyle(
+        double sectionsOuter, double sectionsWidth, PieSeries<ObservableValue> series)
+        {
+            series.OuterRadiusOffset = sectionsOuter;
+            series.MaxRadialColumnWidth = sectionsWidth;
         }
 
         private void UpdateChart(string payload)
@@ -89,6 +109,7 @@ namespace SmartHomeMonitoringApp.Views
                 var temp = Convert.ToDouble(sqlitValue[0]);
                 var humid = Convert.ToDouble(sqlitValue[1]);
 
+                // 온도차트 값
                 var tempVal = GaugeGenerator.BuildSolidGauge(new GaugeItem(
                     temp,
                     series =>
@@ -97,8 +118,50 @@ namespace SmartHomeMonitoringApp.Views
                         series.DataLabelsSize = 50;
                     }
                 ));
-                ChtDinningTemp.Series = tempVal;
+                ChtLivingTemp.Series = ChtDiningTemp.Series = ChtBedTemp.Series = ChtBathTemp.Series = tempVal;
+
+                var sectionsOuter = 130;
+                var sectionsWidth = 20;
+
+                // 습도차트 값
+                HumidSeries = GaugeGenerator.BuildAngularGaugeSections(
+                    new GaugeItem(humid,
+                    s => SetStyle(sectionsOuter, sectionsWidth, s))
+                );
+                ChtLivingHumid.Series = ChtDiningHumid.Series = ChtBedHumid.Series = ChtBathHumid.Series = HumidSeries;
+
+                // 습도를 나타낼 앵귤러차트를 초기화
+                Needle = new NeedleVisual { Value = humid };
+                VisualElements = new VisualElement<SkiaSharpDrawingContext>[]
+                {
+                new AngularTicksVisual
+                {
+                    LabelsSize = 12,
+                    LabelsOuterOffset = 15,
+                    OuterOffset = 65,
+                    TicksLength = 15
+                },
+                Needle
+                };
+
+                ChtLivingHumid.VisualElements = ChtDiningHumid.VisualElements = VisualElements;
+                ChtBedHumid.VisualElements = ChtBathHumid.VisualElements = VisualElements; // 위에서 만든 화면디자인을 차트에 적용
             });
+        }
+
+        private void BtnWarning_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'control':'warning'}");
+        }
+
+        private void BtnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'control':'normal'}");
+        }
+
+        private void BtnOff_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'control':'off'}");
         }
     }
 }
--- part2/win/SmartHomeMonitor/Views/VisualizationControl.xaml.cs
+++ part2/win/SmartHomeMonitor/Views/VisualizationControl.xaml.cs
@@ -1,7 +1,10 @@
-﻿using SmartHomeMonitoringApp.Logics;
+﻿using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using SmartHomeMonitoringApp.Logics;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -30,11 +33,52 @@ namespace SmartHomeMonitoringApp.Views
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
+            DtpStart.Text = DateTime.Now.AddDays(-2).ToString("yyyy-MM-dd");
+            DtpEnd.Text = DateTime.Now.ToString("yyyy-MM-dd");
         }
 
         // 검색버튼 클릭 이벤트 핸들러
         private async void BtnSearch_Click(object sender, RoutedEventArgs e)
         {
+            DataSet ds = new DataSet(); // 실제 데이터를 담을 객체
+
+            using (SqlConnection conn = new SqlConnection(Commons.DBCONNSTRING))
+            {
+                conn.Open();
+
+                var selQuery = @"SELECT [Idx]
+                                      , [DEV_ID]
+                                      , [CURR_DT]
+                                      , [TEMP]
+                                      , [HUMID]
+                                   FROM [dbo].[SmartHomeData]
+                                  WHERE CONVERT(CHAR(10),CURR_DT, 23) BETWEEN @StartDt AND @EndDt";
+
+                SqlCommand cmd = new SqlCommand(selQuery, conn);
+                cmd.Parameters.AddWithValue("@StartDt", DtpStart.Text);
+                cmd.Parameters.AddWithValue(@"EndDt", DtpEnd.Text);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(ds, "SmartHomeData");
+            }
+
+            //MessageBox.Show("TotalData", ds.Tables["SmartHomeData"].Rows.Count.ToString());
+            LblTotalCount.Content = $"검색데이터 : {ds.Tables["SmartHomeData"].Rows.Count} 개";
+
+            var title = new LabelVisual
+            {
+                Text = "SmartHome Visualization",
+                TextSize = 16,
+                Padding = new LiveChartsCore.Drawing.Padding(15),
+            };
+            ChtResult.Title = title;
+
+            var series = new LineSeries<double> { Fill = null };
+            foreach (DataRow item in ds.Tables["SmartHomeData"].Rows)
+            {
+                series.Values.Append(Convert.ToDouble(item[4]));
+            }
+            ChtResult.Series = series;
         }
     }
 }
